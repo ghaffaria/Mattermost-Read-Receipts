a893e90042d9c507c9ae1b268de17f0f49fa24ae
Alireza Ghaffari <arg@pardisco.co>
2025-05-20 22:40:44 +0330

    Simplifies PostReceipt component registration
    Refactors the registration of the PostReceipt component by removing redundant wrapper elements and streamlining the code. Enhances maintainability and readability while preserving functionality.

--- webapp/plugin.tsx
+++ webapp/plugin.tsx
@@ -5,15 +5,9 @@ import PostReceipt from './components/PostReceipt';
 export default class ReadReceiptPlugin {
     initialize(registry: PluginRegistry) {
         // Register PostReceipt to render after the message body.
-        registry.registerPostTypeComponent('custom_read_receipt', (props: { post: { id: string }; children?: React.ReactNode }) => {
-            const { post, children } = props;
-            return (
-                <>
-                    {children}
-                    <PostReceipt messageId={post.id} />
-                </>
-            );
-        });
+        registry.registerPostTypeComponent(({post}) => (
+            <PostReceipt messageId={post.id} />
+        ));
 
         console.log('ReadReceiptPlugin initialized and PostReceipt registered.');
     }
